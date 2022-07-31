c3d4351016836402da15312bc2805654740ff343
neeladri22 <neeladri22@gmail.com>
2022-07-31 10:46:33 +0530

    /Find numbers like 33,77 etc between 0-100...RG-----

--- JavaScriptProgram.js
+++ JavaScriptProgram.js
@@ -1,28 +1,11 @@
-//three elements whose sum is equal to zero
-arr = [0, -1, 2, -3, 1];
-      
-// Prints all triplets in arr[] with 0 sum    
-    function findTriplets(arr) {
-        let found = false;
-        for (let i = 0; i < arr.length - 2; i++) {
-        for (let j = i + 1; j < arr.length - 1; j++) {
-            for (let k = j + 1; k < arr.length; k++) {
-                if (arr[i] + arr[j] + arr[k] === 0)
-                {
-                    console.log("Three Numbers Whose Sum equal to Zero");
-                    console.log(arr[i]);
-                    console.log(arr[j]);
-                    console.log(arr[k]);
-                    console.log(".....................");
-                    found = true;
-                     
-                }
-            }
-        }
-        // If no triplet with 0 sum found in array
-        if(found === false) {
-            document.write(" not exist ");
-        }
-    }
-   }
-    findTriplets(arr);
+//Find numbers like 33,77 etc between 0-100...RG-----
+function isRepeated(number) {
+    if (number % 11 == 0) return true;
+    else return false;
+}
+  
+let repeatedNumbersArray = new Array();
+for (let i = 1; i < 100; i++) {
+    if (isRepeated(i)) repeatedNumbersArray.push(i);
+}
+  console.log(repeatedNumbersArray);
